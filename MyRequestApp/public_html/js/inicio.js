ba15c9d222e8bebb5d2b64f821175a22fc86a056
abelo003 <boxos.live@hotmail.com>
2018-02-12 17:32:40 -0600

    Se agrega la funcionalidad de angularjs y bootstrap

---
+++ MyRequestApp/public_html/js/inicio.js
@@ -0,0 +1,33 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var myAngularApp = angular.module('MyRequestApp', ['ngRoute']).directive('test', function() {
+});
+
+//Información general
+myAngularApp.controller('InicioController', function ($scope) {
+    
+    $scope.archivoActual = {
+        nombre: "Jimbo"
+    };
+    
+    $scope.fnInitArchivo = function(nombre, tamano, fechaCreacion){
+        if(nombre === ""){
+            $scope.archivoActual = null;
+            $('#modalSeleccionArchivo').modal('show');
+        }
+        else{
+            $scope.archivoActual = {
+                nombre: nombre,
+                tamano: tamano,
+                fechaCreacion: fechaCreacion
+            };
+        }
+    };
+    
+    
+    
+});
