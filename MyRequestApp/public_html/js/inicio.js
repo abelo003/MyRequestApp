247e9dac38d2f7de5d4e8088dafdd1d34f070a35
abelo003 <boxos.live@hotmail.com>
2018-02-13 13:27:11 -0600

    Se redefinen los parametros

--- MyRequestApp/public_html/js/inicio.js
+++ MyRequestApp/public_html/js/inicio.js
@@ -4,28 +4,53 @@
  * and open the template in the editor.
  */
 
-$(function(){
+$(function () {
     $('.carousel').carousel({
         interval: 5000
     });
+    $('#modalPregunta').modal({
+        backdrop: 'static',
+        keyboard: false,
+        show: false
+    });
+
+    var modalConfirm = function (callback) {
+
+        $("#modal-btn-si").on("click", function () {
+            callback(true);
+        });
+
+        $("#modal-btn-no").on("click", function () {
+            callback(false);
+        });
+    };
+
+    modalConfirm(function (confirm) {
+        if (confirm) {
+            //Acciones si el usuario confirma
+            alert("Oh Yeha!!!");
+        } else {
+            //Acciones si el usuario no confirma
+            alert("No");
+        }
+    });
 });
 
-var myAngularApp = angular.module('MyRequestApp', ['ngRoute']).directive('test', function() {
+var myAngularApp = angular.module('MyRequestApp', ['ngRoute']).directive('test', function () {
 });
 
 //Información general
 myAngularApp.controller('InicioController', function ($scope) {
-    
+
     $scope.archivoActual = {
         nombre: "Jimbo"
     };
-    
-    $scope.fnInitArchivo = function(nombre, tamano, fechaCreacion){
-        if(nombre === ""){
+
+    $scope.fnInitArchivo = function (nombre, tamano, fechaCreacion) {
+        if (nombre === "") {
             $scope.archivoActual = null;
             $('#modalSeleccionArchivo').modal('show');
-        }
-        else{
+        } else {
             $scope.archivoActual = {
                 nombre: nombre,
                 tamano: tamano,
@@ -33,7 +58,7 @@ myAngularApp.controller('InicioController', function ($scope) {
             };
         }
     };
-    
-    
-    
+
+
+
 });
